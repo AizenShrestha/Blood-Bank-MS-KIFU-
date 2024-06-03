8669e1223a31ecb124c0143d7447e00a944a7434
unknown <shresthaaizen25@gmail.com>
2024-06-03 08:25:49 +0545

    serverupdated

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,39 @@
 const express = require('express')
+const dbConnect = require('./src/db/connection')
+dbConnect()
 const app = express()
-const port = 5000
+require('dotenv').config()
+app.use(express.json())
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
 
-app.get('/me', (req, res) => {
-  res.send({
-    name:"Aizen",
-    balance: 50000000,
-    rewardPoints: 460
-  })
+const userSchema = new Schema({
+  name: String,
+  phonenumber: String,
+  addr: String,
+  dob: String,
+  bloodgroup: {
+    type: String,
+    enum: ['O+','O-','A+','A-','B+','B-'],
+    default: "null"
+  }
+});
+
+
+const User = mongoose.model('User', userSchema);
+const port = process.env.PORT
+
+
+app.post('/Register', async(req, res) => {
+ /// User.create({name:"HEHE", addr:"BTM"})
+ console.log(req.body.name)
+  User.create(req.body)
+  res.send('ok')
+})
+
+app.get('/Register', async (req, res) => {
+  const data = await User.find()
+  res.send(data)
 })
 
 app.listen(port, () => {
